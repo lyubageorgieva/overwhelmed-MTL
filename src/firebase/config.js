bb6c5304cb0af3b3166058ddfb1e1f0c0eb22101
Bryce Nichol <71189882+bryce-n-dev@users.noreply.github.com>
2022-01-22 02:55:00 -0500

    Add react nativation and firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxaDuJFWou3Ac7DNlNKonAvO12AzX6KfQ",
+  authDomain: "uchange-1c51c.firebaseapp.com",
+  projectId: "uchange-1c51c",
+  storageBucket: "uchange-1c51c.appspot.com",
+  messagingSenderId: "959111341797",
+  appId: "1:959111341797:web:78d64edc30e4692b0f560c",
+  measurementId: "G-4ZP069MLC6"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };

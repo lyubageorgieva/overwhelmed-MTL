e1dfcf29dcb734708d448c12f92e9ab1d80b0d12
Bryce Nichol <71189882+bryce-n-dev@users.noreply.github.com>
2022-01-23 01:02:04 -0500

    auth perm, firebase emergency

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,12 @@ import "firebase/compat/firestore";
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxaDuJFWou3Ac7DNlNKonAvO12AzX6KfQ",
-  authDomain: "uchange-1c51c.firebaseapp.com",
-  projectId: "uchange-1c51c",
-  storageBucket: "uchange-1c51c.appspot.com",
-  messagingSenderId: "959111341797",
-  appId: "1:959111341797:web:78d64edc30e4692b0f560c",
-  measurementId: "G-4ZP069MLC6"
+  apiKey: "AIzaSyBmV4URImTgNXVllSKcMGPgtAdHII7ckws",
+  authDomain: "uchange2-721d1.firebaseapp.com",
+  projectId: "uchange2-721d1",
+  storageBucket: "uchange2-721d1.appspot.com",
+  messagingSenderId: "670533101083",
+  appId: "1:670533101083:web:5c32f0967fb1cf3da7fabe"
 };
 
 if (!firebase.apps.length) {
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -2,74 +2,73 @@ import { StyleSheet, Image, View } from 'react-native';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Divider, Text, Button } from '@ui-kitten/components';
 import React from 'react';
-import { useState, useEffect} from 'react';
-import { getAuth } from 'firebase/auth';
-
+import { useState, useEffect } from 'react';
+import { signOut, getAuth } from 'firebase/auth';
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 const db = firebase.firestore();
 
-const ProfileScreen = ({navigation}) => {
-const [user, setUser] = useState({});
+const ProfileScreen = ({ navigation }) => {
+  const [user, setUser] = useState({});
 
-const getUser = async() => { 
+  const getUser = async () => {
 
-  db.collection("users").where("id", "==", firebase.auth().currentUser.uid)
-    .get()
-    .then((querySnapshot) => {
-      let currentUser = {}
+    db.collection("users").where("id", "==", firebase.auth().currentUser.uid)
+      .get()
+      .then((querySnapshot) => {
+        let currentUser = {}
         querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            currentUser = doc.data()
+          // doc.data() is never undefined for query doc snapshots
+          currentUser = doc.data()
         });
         setUser(currentUser)
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.log("Error getting documents: ", error);
-    })
+      })
   }
-useEffect(() => {
+  useEffect(() => {
     getUser();
   }, []);
 
   return (
     <View >
       <View style={styles.header}></View>
-        <Image style={styles.avatar} source={{uri: 'http://www.gravatar.com/avatar/?d=mp'}}/>
-        <View style={styles.content}>
-          <Text style={styles.titleStyle} category='h3'>{user.username}</Text>
+      <Image style={styles.avatar} source={{ uri: 'http://www.gravatar.com/avatar/?d=mp' }} />
+      <View style={styles.content}>
+        <Text style={styles.titleStyle} category='h3'>{user.username}</Text>
+      </View>
+
+      <View style={styles.body}>
+        <View style={styles.fields}>
+          <Text style={styles.fieldStyle} category='h5'>City</Text>
+          <Text style={styles.fieldStyle} category='s1'>{user.city}</Text>
         </View>
 
-        <View style={styles.body}>
-          <View style={styles.fields}>
-            <Text style={styles.fieldStyle} category='h5'>City</Text>
-            <Text style={styles.fieldStyle} category='s1'>{user.city}</Text>
-          </View>
+        <View style={styles.fields}>
+          <Text style={styles.fieldStyle} category='h5'>Major</Text>
+          <Text style={styles.fieldStyle} category='s1'>{user.major}</Text>
+        </View>
 
-          <View style={styles.fields}>
-            <Text style={styles.fieldStyle} category='h5'>Major</Text>
-            <Text style={styles.fieldStyle} category='s1'>{user.major}</Text>
-          </View>
+        <View style={styles.fields}>
+          <Text style={styles.fieldStyle} category='h5'>Home University</Text>
+          <Text style={styles.fieldStyle} category='s1'>{user.university}</Text>
+        </View>
 
-          <View style={styles.fields}>
-            <Text style={styles.fieldStyle} category='h5'>Home University</Text>
-            <Text style={styles.fieldStyle} category='s1'>{user.university}</Text>
-          </View>
+        <View style={styles.fields}>
+          <Text style={styles.fieldStyle} category='h5'>Host University</Text>
+          <Text style={styles.fieldStyle} category='s1'>{user.host}</Text>
+        </View>
 
-          <View style={styles.fields}>
-            <Text style={styles.fieldStyle} category='h5'>Host University</Text>
-            <Text style={styles.fieldStyle} category='s1'>{user.host}</Text>
+        <View style={styles.buttonsAllign}>
+          <View>
+            <Button style={styles.matchUpButton} appearance='ghost'> <Text style={styles.text}>Match</Text></Button>
           </View>
-
-          <View style={styles.buttonsAllign}>
-            <View>
-              <Button style={styles.matchUpButton} appearance='ghost'> <Text style={styles.text}>Match</Text></Button>
-            </View>
-            <View >
-              <Button style={styles.matchUpButton} appearance='ghost'> <Text style={styles.text}>Logout</Text></Button>
-            </View>
+          <View >
+            <Button style={styles.matchUpButton} appearance='ghost' onPress={() => signOut(getAuth())}> <Text style={styles.text}>Logout</Text></Button>
           </View>
         </View>
+      </View>
     </View>
   );
 };
@@ -130,7 +129,7 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   buttonsAllign: {
-    flexDirection:"row",
+    flexDirection: "row",
     justifyContent: 'center',
   },
 });

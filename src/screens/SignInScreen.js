3b766b68fc24fd428491b4ad33e3e5b0a5e337b4
Bryce Nichol <71189882+bryce-n-dev@users.noreply.github.com>
2022-01-22 21:40:40 -0500

    Sign in with firebase

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,21 +1,39 @@
 import { StyleSheet, TextInput } from 'react-native';
 import * as eva from '@eva-design/eva';
-import {ApplicationProvider, Button, Layout, Text, Input } from '@ui-kitten/components';
+import { ApplicationProvider, Button, Layout, Text, Input } from '@ui-kitten/components';
 import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const SignInScreen = ({navigation}) => {
+const SignInScreen = ({ navigation }) => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [secureTextEntry, setSecureTextEntry] = React.useState(true);
+
+  const onSignIn = () => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        navigation.navigate('Profile')
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error)
+      });
+  }
+
   return (
     <Layout style={styles.content}>
-      <Text  style={styles.titleStyle} category='h3'>Sign in</Text>
-      <Input 
+      <Text style={styles.titleStyle} category='h3'>Sign in</Text>
+      <Input
         style={styles.inputStyle}
         placeholder='Enter your email address'
-        email = {email}
+        email={email}
         label='Email'
-        onChangeText={nextEmail => setEmail(nextEmail)}
+        onChangeText={setEmail}
       />
       <Input
         style={styles.inputStyle}
@@ -23,9 +41,9 @@ const SignInScreen = ({navigation}) => {
         label='Password'
         placeholder='Enter your password'
         secureTextEntry={secureTextEntry}
-        onChangeText={nextPassword => setPassword(nextPassword)}
+        onChangeText={setPassword}
       />
-      <Button style={styles.signInButton}>SIGN IN</Button>
+      <Button style={styles.signInButton} onPress={onSignIn}>SIGN IN</Button>
       <Text>Do not have an account? <Text style={styles.signUpButton} onPress={() => navigation.navigate('Sign Up')}> Sign up</Text></Text>
     </Layout>
   );
@@ -50,9 +68,9 @@ const styles = StyleSheet.create({
   },
 
   content: {
-    alignItems:'center',
-    justifyContent:'center',
-    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
     padding: 10,
   },
 

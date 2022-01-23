f4b6653ff120974191c7366b7c30e5ba99cca462
Bryce Nichol <71189882+bryce-n-dev@users.noreply.github.com>
2022-01-23 03:00:48 -0500

    emergency 2 lol

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,21 @@ import "firebase/compat/firestore";
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmV4URImTgNXVllSKcMGPgtAdHII7ckws",
-  authDomain: "uchange2-721d1.firebaseapp.com",
-  projectId: "uchange2-721d1",
-  storageBucket: "uchange2-721d1.appspot.com",
-  messagingSenderId: "670533101083",
-  appId: "1:670533101083:web:5c32f0967fb1cf3da7fabe"
+
+  apiKey: "AIzaSyCgCW67WQVX5OHM-FDHWPHauKByI3olsSM",
+
+  authDomain: "uchange3-98b61.firebaseapp.com",
+
+  projectId: "uchange3-98b61",
+
+  storageBucket: "uchange3-98b61.appspot.com",
+
+  messagingSenderId: "1041434161742",
+
+  appId: "1:1041434161742:web:a5f8e5b43f6ac089f1891f"
+
 };
+
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
